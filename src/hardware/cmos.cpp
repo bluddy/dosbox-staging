a843dbb42591334c342105a888ebf1114e7747e2
Yotam Barnoy <yotambarnoy@gmail.com>
2025-08-27 14:12:25 +0300

    Refactor: Decouple cmos.cpp from dosbox.h

--- src/hardware/cmos.cpp
+++ src/hardware/cmos.cpp
@@ -1,7 +1,10 @@
 // SPDX-FileCopyrightText:  2002-2021 The DOSBox Team
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "dosbox.h"
+#include "dosbox_config.h"
+#include "misc/types.h"
+#include "misc/logging.h"
+#include "dosbox_app.h"
 
 #include <cmath>
 #include <ctime>
